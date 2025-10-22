72334912e5f98c3a4f7d4b20e7059b4e64795ce8
aounbhutta12-star <aounbhutta12@gmail.com>
2025-10-22 15:10:14 +0500

    Enhance guess input validation and error handling
    Added input validation and error handling for user guesses.

--- guess_the_number.py
+++ guess_the_number.py
@@ -9,18 +9,34 @@ attempt=0
 
 
 while True:
-    
-    n=int(input("Guess the number between 1 and 100: "))
-   
-    attempt +=1
-    
-    if(n>guess):
-        print("Too High! Guess a lower number ")
-  
-    elif(n<guess):
-        print("Too Low")
+    try:
+       
+        n=int(input("Guess the number between 1 and 100: "))
+
         
-    elif(n==guess):
-        print(f"Yes you guessed the  number ({guess}) in ({attempt}) attempt")
-        break
+        if n < 1 or n > 100:
+           
+            print("Please enter a number BETWEEN 1 and 100 only!")
+            
+            continue 
+        
+        
+        attempt +=1
+        
+        if(n>guess):
+            print("Too High! Guess a lower number ")
+    
+        elif(n<guess):
+            print("Too Low")
+            
+        elif(n==guess):
+            print(f"Yes you guessed the  number ({guess}) in ({attempt}) attempt")
+            break
+
+            
+    except ValueError:
+
+        print("ENTER A VALID NUMBER PLEASE")
+        
+
 
